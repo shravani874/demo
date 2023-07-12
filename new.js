ef9ee4341ff2599f6b6a9e168739d371486496b3
shra <shravani.pixeldart@gmail.com>
2023-07-12 17:19:16 +0530

    js

--- new.js
+++ new.js
@@ -29,12 +29,14 @@ let a = prompt("Enter the first variable: ");
 let b = prompt("Enter the second variable: ");
 
 //create a temporary variable
-let temp;
+//JavaScript program to swap two variables
 
-//swap variables
-temp = a;
-a = b;
-b = temp;
+//take input from the users
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//using destructuring assignment
+[a, b] = [b, a];
 
 console.log(`The value of a after swapping: ${a}`);
 console.log(`The value of b after swapping: ${b}`);

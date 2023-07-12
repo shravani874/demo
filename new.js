bf0221f63b216f9644c57de5908371760b9e975f
shra <shravani.pixeldart@gmail.com>
2023-07-12 17:41:00 +0530

    here

--- new.js
+++ new.js
@@ -45,3 +45,17 @@ let d = prompt("Enter the second variable: ");
 
 console.log(`The value of a after swapping: ${a}`);
 console.log(`The value of b after swapping: ${b}`);
+
+//JavaScript program to swap two variables
+
+//take input from the users
+let al = prompt("Enter the first variable: ");
+let f = prompt("Enter the second variable: ");
+
+// XOR operator
+a = a ^ b;
+b = a ^ b;
+a = a ^ b;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
